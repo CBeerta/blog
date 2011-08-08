1f5668326d90608dc219e3375f15f6b18d8d627e
Claus Beerta <claus@beerta.de>
2011-08-08 20:48:48 +0200

    That is alot prettier now.

--- controllers/docs.php
+++ controllers/docs.php
@@ -42,76 +42,111 @@
 **/
 class Docs
 {
-
     /**
-    * Documentation or 'Brain Dump' Index page
-    *
-    * @param string $slug filename of file to show
-    *
-    * @return html
-    *
-    * @FIXME: this will get slow with lots of files. fortunetly i'll never ever
-    *         write nearly enough docs for this to ever bevome a problem. EVER
+    * loadDocs stores the most up-to-date doc_name here
     **/
-    public static function index($slug = null)
+    private static $_latest = null;
+    
+    /**
+    * Load all Documents and store info aswell as find latest
+    *
+    * @return array
+    **/
+    public static function loadDocs()
     {
-        $topics = array();
-        $sub_topics = array();
+        $docs = array();
         $file_timestamps = array();
  
         $docs_dir = Slim::config('docs_dir');
         
         $glob = "{{$docs_dir}/*.html,{$docs_dir}/*.mkd}";
         foreach (glob($glob, GLOB_BRACE) as $filename) {
-
+        
             // Don't want unreadable files
             if (!is_readable($filename)) {
                 continue;
             }
 
-            preg_match('#.*/(.*?)\.(\w+)$#', $filename, $matches);
-            $topic = $matches[1];
-            $topics[] = $topic;
+            $basename = basename($filename);
+            $topics = array();
 
+            preg_match('#(.*?)\.(\w+)$#', $basename, $matches);
+            $doc_name = $matches[1];
+            
             // load file
             $data = file($filename);
 
-            // if this is the one, then keep it
-            if ($slug == $topic) {
-                $content = Markdown(implode("", $data));
-            }
-            
             // load topics from the file
             foreach ($data as $line) {
                 if (!preg_match('|^#(\s)?(.*)$|', $line, $matches)) {
                     continue;
                 }
-                $sub_topics[$topic][] = $matches[2];
+                $topics[] = $matches[2];
             }
-            
-            // 
+            unset($data);
+
+            // store timestamps
             $stat = stat($filename);
-            $file_timestamps[$stat['mtime']] = $filename;
-        }        
+            $file_timestamps[$stat['mtime']] = $doc_name;
+
+            $docs[$doc_name] = (object) array(
+                'filename' => $filename,
+                'doc_name' => $doc_name,
+                'topics' => $topics,
+                'stat' => $stat,
+                'content' => 'to-be-loaded',
+            );
+            
+        }
         
-        // No slug selected, so pick whatever was edited most recently        
+        // Store the most recent document title
+        ksort($file_timestamps);
+        self::$_latest = array_pop($file_timestamps);
+        
+        return $docs;
+    }
+    
+    /**
+    * Load the actual Content for this document
+    *
+    * @param object $doc Doc to load content for
+    *
+    * @return object
+    **/
+    public static function loadContent($doc)
+    {
+        $data = file_get_contents($doc->filename);
+        $doc->content = Markdown($data);
+        
+        return $doc;
+    }
+    
+    /**
+    * Documentation or 'Brain Dump' Index page
+    *
+    * @param string $slug filename of file to show
+    *
+    * @return html
+    **/
+    public static function index($slug = null)
+    {
+        $docs = self::loadDocs();
+        
         if ($slug === null) {
-            ksort($file_timestamps);
-            $filename = array_pop($file_timestamps);
-            $content = Markdown(file_get_contents($filename));
+            $content = $docs[self::$_latest];
+        } else {
+            $content = $docs[$slug];
         }
         
         Slim::view()->appendData(
             array(
-                'topics' => $topics,
-                'sub_topics' => $sub_topics,
-                'content' => $content,
+                'docs' => $docs, 
+                'display' => self::loadContent($content),
             )
         );
         
         return Slim::render('docs.html');
     }
-
 
 }
 
