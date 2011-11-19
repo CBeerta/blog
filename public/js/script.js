9b7c62fc5291fdafadf37d79de91803b3990c881
Claus Beerta <claus@beerta.de>
2011-11-19 12:13:07 +0100

    move analytics js to scripts, add g+1 button

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,25 @@
 
 */
 
+/* google+ buttan */
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+
+/* google analytics */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-26492666-1']);
+_gaq.push(['_setDomainName', 'beerta.net']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
 $(document).ready(function() 
 {
